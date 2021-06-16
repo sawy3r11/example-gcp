b612c823d68724839ec0326f3c3bec5983bf8506
dawid <sawy3r11@gmail.com>
2021-06-16 19:45:10 +0200

    Initial commit

---
+++ example-gcp/src/main/java/com/dk/examplegcp/ExampleGcpApplication.java
@@ -0,0 +1,13 @@
+package com.dk.examplegcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleGcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleGcpApplication.class, args);
+	}
+
+}
---
+++ example-gcp/src/test/java/com/dk/examplegcp/ExampleGcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dk.examplegcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleGcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
